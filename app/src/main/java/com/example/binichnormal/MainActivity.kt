b561c4e8d13be9d6bff0873974172e2e0b010b65
Lucmie98 <lucmie98@gmail.com>
2020-04-07 03:32:01 +0200

    updating everything to the guideline style

--- app/src/main/java/com/example/binichnormal/MainActivity.kt
+++ app/src/main/java/com/example/binichnormal/MainActivity.kt
@@ -7,6 +7,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_menu)
+        setContentView(R.layout.activity_main_menu)
     }
 }
